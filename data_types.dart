836da2dbe908c17124d30a727ffaa4d885c38c8a
Ebaq003 <159926895+Ebaq003@users.noreply.github.com>
2024-03-12 22:31:24 +0300

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,19 @@
+void main(){
+  //data types
+  //string 
+  String name = "Ebaq";
+  //boolean
+  bool female = true;
+  //integers
+  int age = 20;
+  //lists
+  List<String> fruits =["mango","apple","melon"];
+  //maps
+  Map<String, dynamic> myData ={
+    "name": name,
+    "age": age,
+    "female": female,
+    "fruits": fruits,
+  };
+  print(myData);
+}
